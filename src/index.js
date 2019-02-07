f35425eef2b115448a471e3ea8ef88041f4f1f32
dbe <davidbrianethier@gmail.com>
2019-02-07 09:22:28 -0700

    Handling invalid pks

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ function receiveMessage(event) {
 function postDetails(event) {
   let details = document.getElementById('details');
   let pk = localStorage.getItem('metaPrivateKey')
+  try {
+    let wallet = new ethers.Wallet(pk)
 
-  //TODO: Make this a more robust pk check
-  if(pk) {
     details.innerHTML = `
       <div>
         <p>${event.data.name} - ${event.origin}</p>
@@ -25,23 +25,23 @@ function postDetails(event) {
         <button id="confirm" style="background-color: green; color:white;width:100px;height:50px;">Allow</button>
       </div>
     `
-  } else {
+    document.getElementById('confirm').addEventListener('click', function() {
+      try {
+        let wallet = new ethers.Wallet(pk)
+        wallet.signMessage(`login-with-burner:${event.data.challenge}`).then(signature => {
+          event.source.postMessage({command: 'signed', signature: signature, address: wallet.address}, '*')
+          window.close()
+        })
+      } catch(e) {
+        event.source.postMessage({command: 'error', message: e}, '*')
+      }
+    })
+  } catch(e) {
+    console.log('e: ', e);
     details.innerHTML = `
       <div>
         <p>You don't have a burner wallet setup yet.</p>
       </div>
     `
   }
-
-  document.getElementById('confirm').addEventListener('click', function() {
-    try {
-      let wallet = new ethers.Wallet(pk)
-      wallet.signMessage(`login-with-burner:${event.data.challenge}`).then(signature => {
-        event.source.postMessage({command: 'signed', signature: signature, address: wallet.address}, '*')
-        window.close()
-      })
-    } catch(e) {
-      event.source.postMessage({command: 'error', message: e}, '*')
-    }
-  })
 }
